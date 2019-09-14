2f1f4e432d1957085eb45a187db40ad34ee4025e
xarielx <a.barboza@ufl.edu>
2019-09-14 00:52:05 -0400

    All bootcamp requirements are met, config file ignored and submitted to canvas

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,23 +1,36 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
-    Schema = mongoose.Schema;
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
 
 /* Create your schema for the data in the listings.json file that will define how data is saved in your database
      See https://mongoosejs.com/docs/guide.html for examples for creating schemas
      See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
   */
 var listingSchema = new Schema({
-  /* Your code for a schema here */ 
-  //Check out - https://mongoosejs.com/docs/guide.html
-
+  code: { type: String, required: true },
+  name: { type: String, required: true },
+  coordinates: [{ latitude: Number, longitude: Number }],
+  address: String,
+  created_at: Date,
+  updated_at: Date
 });
-
 /* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
    See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 */
-listingSchema.pre('save', function(next) {
-  /* your code here */
+listingSchema.pre('save', function (next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
 });
+
 
 /* Use your schema to instantiate a Mongoose model */
 //Check out - https://mongoosejs.com/docs/guide.html#models
@@ -25,3 +38,5 @@ var Listing = mongoose.model('Listing', listingSchema);
 
 /* Export the model to make it avaiable to other parts of your Node application */
 module.exports = Listing;
+
+
--- queries.js
+++ queries.js
@@ -1,36 +1,71 @@
 /* Add all the required libraries*/
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
+var fs = require('fs'),
+  mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  Listing = require('./ListingSchema.js'),
+  config = require('./config');
+mongoose.connect(config.db.uri, { useNewUrlParser: true });
+const util = require('util')
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
-var findLibraryWest = function() {
-  /* 
+var findLibraryWest = function () {
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+  async function find() {
+    const listing = await Listing.find({ name: 'Library West' });
+    console.log(listing);
+  }
+  find();
+
 };
-var removeCable = function() {
+var removeCable = function () {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+  async function remove() {
+    const listing = await Listing.findOneAndDelete({ code: 'CABL' });
+    if (listing === null) {
+      console.log("Listing was not found or it has already  been deleted");
+    } else {
+      console.log(listing);
+    }
+  }
+  remove();
 };
-var updatePhelpsLab = function() {
+var updatePhelpsLab = function () {
   /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Lab address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
+  Listing.findOneAndUpdate({ "name": "Phelps Laboratory" }, { $set: { address: "1953 Museum Road Gainesville, FL 32611" } }, { returnOriginal: false }, (err, data) => {
+    if (err) {
+      console.log("Something wrong when updating data!");
+    }
+    console.log(data);
+  });
 };
-var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+var retrieveAllListings = function () {
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+  util.inspect.defaultOptions.maxArrayLength = null; //this enables the  console to display the
+  Listing.find(function (err, data) {
+    if (err) return handleError(err);
+    console.log(data);
+
+  })
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
+//
